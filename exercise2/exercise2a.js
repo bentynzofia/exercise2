6b0f7cc627bd944c7f4670bb918ad9efa742ba07
bentynzofia <zofia.bentyn@gmail.com>
2020-07-28 13:42:01 +0200

    Liczby z listy do tablicy i suma nieparzystych

--- exercise2/exercise2a.js
+++ exercise2/exercise2a.js
@@ -1,10 +1,13 @@
-var utilisModule = require('./utilis.js');
+/*jshint esversion: 6 */
+const utilisModule = require('./utilis.js');
 
 fs = require('fs');
 fs.readFile('./numbers.txt', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-
-utilisModule.splitByNewLine(data);
+const numbersArray = utilisModule.splitByNewLine(data);
+console.log(numbersArray);
+const sumOfOdds = utilisModule.countOdd(numbersArray);
+console.log(sumOfOdds);
 });
--- exercise2/utilis.js
+++ exercise2/utilis.js
@@ -1,16 +1,15 @@
 /*jshint esversion: 6 */
 const utilis = {
   splitByNewLine: fileData => {
-  lines = fileData.split('\n');
-  console.log(lines);
-},
-  countOdd: x => {
-  let oddsSum = 0;
-  if(x%2!==0){
-    oddsSum += x;
+  lines = fileData.split('\r\n');
+  numbers = lines.map(num => Number(num));
+  return numbers;
+  },
+  countOdd: num => {
+    const oddNumbers = num.filter(num => num%2!==0);
+    const oddSum = oddNumbers.reduce((a,b) => a+b);
+    return oddSum;
   }
-  return oddsSum;
-}
 };
 
 module.exports = utilis;
