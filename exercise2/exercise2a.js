5dc5d52df3471af290972a90279ceaa1c72c74a6
bentynzofia <zofia.bentyn@gmail.com>
2020-07-28 13:47:00 +0200

    formatuje pliki, bo proszą

--- exercise2/exercise2a.js
+++ exercise2/exercise2a.js
@@ -1,13 +1,13 @@
 /*jshint esversion: 6 */
-const utilisModule = require('./utilis.js');
+const utilisModule = require("./utilis.js");
 
-fs = require('fs');
-fs.readFile('./numbers.txt', 'utf8', function (err,data) {
+fs = require("fs");
+fs.readFile("./numbers.txt", "utf8", function (err, data) {
   if (err) {
     return console.log(err);
   }
-const numbersArray = utilisModule.splitByNewLine(data);
-console.log(numbersArray);
-const sumOfOdds = utilisModule.countOdd(numbersArray);
-console.log(sumOfOdds);
+  const numbersArray = utilisModule.splitByNewLine(data);
+  console.log(numbersArray);
+  const sumOfOdds = utilisModule.countOdd(numbersArray);
+  console.log(sumOfOdds);
 });
--- exercise2/utilis.js
+++ exercise2/utilis.js
@@ -1,15 +1,15 @@
 /*jshint esversion: 6 */
 const utilis = {
-  splitByNewLine: fileData => {
-  lines = fileData.split('\r\n');
-  numbers = lines.map(num => Number(num));
-  return numbers;
+  splitByNewLine: (fileData) => {
+    lines = fileData.split("\r\n");
+    numbers = lines.map((num) => Number(num));
+    return numbers;
   },
-  countOdd: num => {
-    const oddNumbers = num.filter(num => num%2!==0);
-    const oddSum = oddNumbers.reduce((a,b) => a+b);
+  countOdd: (num) => {
+    const oddNumbers = num.filter((num) => num % 2 !== 0);
+    const oddSum = oddNumbers.reduce((a, b) => a + b);
     return oddSum;
-  }
+  },
 };
 
 module.exports = utilis;
