fbaa27e7567f07354f935ce19725b2beeeae85ad
bentynzofia <zofia.bentyn@gmail.com>
2020-07-30 09:24:41 +0200

    fsPromises

---
+++ exercise2/bonus.js
@@ -0,0 +1,18 @@
+//jshint esversion: 8
+const utilisModule = require("./utilis.js");
+
+const fs = require('fs');
+const fsPromises = fs.promises;
+
+async function showFile(file) {
+  const content = await fsPromises.readFile(file, 'utf-8');
+  const numbersArray = utilisModule.splitByNewLine(content.toString());
+  const amountOfOdds = utilisModule.countOdd(numbersArray);
+
+  console.log(numbersArray);
+  console.log(amountOfOdds);
+
+  return amountOfOdds;
+}
+
+showFile("./numbers.txt");
