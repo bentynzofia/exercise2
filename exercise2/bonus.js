5abd9866ddc00842ec33129c4adf091bc9d8df3f
bentynzofia <zofia.bentyn@gmail.com>
2020-07-30 09:25:10 +0200

    prettier

--- exercise2/bonus.js
+++ exercise2/bonus.js
@@ -1,11 +1,11 @@
 //jshint esversion: 8
 const utilisModule = require("./utilis.js");
 
-const fs = require('fs');
+const fs = require("fs");
 const fsPromises = fs.promises;
 
 async function showFile(file) {
-  const content = await fsPromises.readFile(file, 'utf-8');
+  const content = await fsPromises.readFile(file, "utf-8");
   const numbersArray = utilisModule.splitByNewLine(content.toString());
   const amountOfOdds = utilisModule.countOdd(numbersArray);
 
